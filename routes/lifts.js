1227f74fe643313924380a19f6af9e5a06feee97
vagrant <vagrant@precise32.(none)>
2014-02-28 23:30:01 +0000

    fixing race

--- routes/lifts.js
+++ routes/lifts.js
@@ -9,16 +9,25 @@ exports.view = function(req, res){
 		if(results[0]) {
 			var lift = results[0].lifts;
 			var lifts = [];
-			for (var i = 0; i < lift.length; i++) {
+			var count = 0;
+			var len = lift.length;
+			for (var i = 0; i < count; i++) {
 				var l = models.Lift.find({"_id": lift[i]}).exec(addToArray);
 				function addToArray(err, toAdd) {
 					if(err) console.log(err);
 					if(toAdd[0]) {
 						lifts.push(toAdd[0]);
-						lifts.sort(function(a,b) { return ((a.name  == b.name) ? 0 : ((a.name>b.name) ? 1 : -1 )); } );
+						count += 1;
+					}
+					else {
+						count += 1;
 					}
 				}
 			}
+			while (count < len) {
+				continue;
+			}
+			lifts.sort(function(a,b) { return ((a.name  == b.name) ? 0 : ((a.name>b.name) ? 1 : -1 )); } );
 			res.render('lifts', { 'lifts': lifts });
 		}
 		else {
