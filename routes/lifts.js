b7675f00a39ff328fd2da415438da16bf1b41e8b
Conner Jevning <cjevning@stanford.edu>
2014-03-02 15:31:49 -0800

    h

--- routes/lifts.js
+++ routes/lifts.js
@@ -3,17 +3,19 @@ var models = require('../models');
 exports.view = function(req, res){
 	var username = req.session.username;
 	var user = models.User.find({"username": username}).exec(afterQuery);
-
+	var count = 0;
 	function afterQuery(err, results) {
 		if(err) console.log(err);
 		if(results[0]) {
 			var lift = results[0].lifts;
 			var lifts = [];
-			var count = 0;
+			
 			var len = lift.length;
-			for (var i = 0; i < count; i++) {
+			console.log(len);
+			for (var i = 0; i < len; i++) {
 				var l = models.Lift.find({"_id": lift[i]}).exec(addToArray);
 				function addToArray(err, toAdd) {
+					console.log(called);
 					if(err) console.log(err);
 					if(toAdd[0]) {
 						lifts.push(toAdd[0]);
@@ -22,9 +24,12 @@ exports.view = function(req, res){
 					else {
 						count += 1;
 					}
+					console.log(count);
 				}
+				
 			}
 			while (true) {
+				//console.log(count);
 				if (count == len-1) break;
 			}
 			lifts.sort(function(a,b) { return ((a.name  == b.name) ? 0 : ((a.name>b.name) ? 1 : -1 )); } );
