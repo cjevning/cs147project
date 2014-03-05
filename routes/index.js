9428bae6392dfb853320f8670002975c55e3ff56
vagrant <vagrant@precise32.(none)>
2014-03-05 05:46:44 +0000

    bug fixed?

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,11 @@
 var nodemailer = require("nodemailer");
 
 exports.view = function(req, res){
-	console.log("=============================================================ORIGINAL+++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 	if (!req.session.username) {
 		req.session.page = "original";
 		if (!req.session.startTime) {
 			req.session.startTime = new Date().getTime();
 			req.session.timeSpent = null;
-			
 		}
 	}
 	else {
@@ -25,7 +23,6 @@ exports.view = function(req, res){
 				req.session.signup = false;
 				type = " sign UP ";
 			}
-			console.log(req.session.page);
 			var mailOptions = {
 			    from: "test@test.com",
 			    to: "cjevning@stanford.edu, juliag1@stanford.edu, buhler@stanford.edu",
@@ -38,7 +35,12 @@ exports.view = function(req, res){
 			        user: "cjevning@gmail.com",
 			        pass: "hz9k1234"
 			    } });
-			
+			transport.sendMail(mailOptions, function(error, response){
+			    if(error){
+			        console.log(error);
+			    }else{
+			        console.log("Message sent: " + response.message);
+			    }});
 		}
 	}
   res.render('index', {
@@ -49,13 +51,11 @@ exports.view = function(req, res){
 };
 
 exports.viewAlt = function(req, res){
-	console.log("=============================================================ALTERNATE+++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 	if (!req.session.username) {
 		req.session.page = "alternate";
 		if (!req.session.startTime) {
 			req.session.startTime = new Date().getTime();
 			req.session.timeSpent = null;
-			
 		}
 	}
 	else {
@@ -69,7 +69,6 @@ exports.viewAlt = function(req, res){
 				req.session.signup = false;
 				type = " sign UP ";
 			}
-			console.log(req.session.page);
 			var mailOptions = {
 			    from: "test@test.com",
 			    to: "cjevning@stanford.edu, juliag1@stanford.edu, buhler@stanford.edu",
@@ -82,7 +81,12 @@ exports.viewAlt = function(req, res){
 			        user: "cjevning@gmail.com",
 			        pass: "hz9k1234"
 			    } });
-			
+			transport.sendMail(mailOptions, function(error, response){
+			    if(error){
+			        console.log(error);
+			    }else{
+			        console.log("Message sent: " + response.message);
+			    }});
 		}
 	}
   res.render('index', {
